f194ef890f24874a492ad6f1eedf9fc8cdad6711
aishwarya <ashp17star@gmail.com>
2024-07-17 12:50:53 +0530

    WebView Implementation, Constraints To Margin, ContainerView functionalities

--- 19_06_24_TabbarAppDemo/EnquiryViewController.swift
+++ 19_06_24_TabbarAppDemo/EnquiryViewController.swift
@@ -6,24 +6,22 @@
 //
 
 import UIKit
+import WebKit
 
 class EnquiryViewController: UIViewController {
 
+    @IBOutlet weak var webView: WKWebView!
+    var url : URL?
+    var urlRequest : URLRequest?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        url = URL(string: "https://bitcode.in/index.html")
+        urlRequest = URLRequest(url: url!)
+        webView.load(urlRequest!)
     }
-    */
-
 }
--- 19_06_24_TabbarAppDemo/InfoViewController.swift
+++ 19_06_24_TabbarAppDemo/InfoViewController.swift
@@ -10,36 +10,101 @@ import UIKit
 class InfoViewController: UIViewController {
 
     @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var androidContainerView: UIView!
+    @IBOutlet weak var iOSContainerView: UIView!
+    @IBOutlet weak var ccppContainerView: UIView!
+    
     private let reuseIdentifierForAndroidViewController = "AndroidInfoViewController"
     private let reuseIdentifierForiOSViewController = "iOSInfoViewController"
     private let reuseIdentifierForCCppInfoViewController = "CCppInfoViewController"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
         initViews()
     }
     
     private func initViews(){
         self.infoLabel.text = "Courses Info"
+        self.androidContainerView.isHidden = false
+        self.ccppContainerView.isHidden = true
+        self.iOSContainerView.isHidden = true
     }
     
-    @IBAction func btnNavigateToAndroidInfoViewController(_ sender: UIButton) {
+        //way 2 --> only one IBAction for three different buttons
     
-        print(sender.tag)
-        let androidInforViewController = self.storyboard?.instantiateViewController(withIdentifier: reuseIdentifierForAndroidViewController) as! AndroidInfoViewController
-    }
-    
-    @IBAction func btnNavigateToiOSInfoViewController(_ sender: UIButton) {
+    @IBAction func btnNavigateBetweenViewControllers(_ sender: UIButton) {
         
-        print(sender.tag)
-        let iOSInfoViewController = self.storyboard?.instantiateViewController(withIdentifier: reuseIdentifierForiOSViewController)
-         as! iOSInfoViewController
+        //way -- using if else conditional statements
+        
+        if (sender.tag == 1){
+            self.androidContainerView.isHidden = false
+            self.iOSContainerView.isHidden = true
+            self.ccppContainerView.isHidden = true
+        } else if (sender.tag == 2){
+            self.iOSContainerView.isHidden = false
+            self.androidContainerView.isHidden = true
+            self.ccppContainerView.isHidden = true
+        } else {
+            self.ccppContainerView.isHidden = false
+            self.androidContainerView.isHidden = true
+            self.iOSContainerView.isHidden = true
+        }
+        
+        
+        //way -- using switch case
+//        switch sender.tag{
+//            case 1 :
+//                self.androidContainerView.isHidden = false
+//                self.iOSContainerView.isHidden = true
+//                self.ccppContainerView.isHidden = true
+//            
+//            case 2 :
+//                self.iOSContainerView.isHidden = false
+//                self.androidContainerView.isHidden = true
+//                self.ccppContainerView.isHidden = true
+//            
+//            case 3 :
+//                self.ccppContainerView.isHidden = false
+//                self.androidContainerView.isHidden = true
+//                self.iOSContainerView.isHidden = true
+//            
+//            default :
+//                self.androidContainerView.isHidden = false
+//                self.ccppContainerView.isHidden = true
+//                self.iOSContainerView.isHidden = true
+//        }
     }
     
-    @IBAction func btnNavigateToCCppInfoViewContorller(_ sender: UIButton) {
-        
-        print(sender.tag)
-        let ccppInfoViewController = self.storyboard?.instantiateViewController(withIdentifier: reuseIdentifierForCCppInfoViewController) as! CCppInfoViewController
-    }
+        //way 1 --> three different actions for three buttons
+    
+//    @IBAction func btnNavigateToAndroidInfoViewController(_ sender: UIButton) {
+//    
+//        print(sender.tag)
+//        
+//        self.androidContainerView.isHidden = false
+//        self.iOSContainerView.isHidden = true
+//        self.ccppContainerView.isHidden = true
+//    }
+//    
+//    @IBAction func btnNavigateToiOSInfoViewController(_ sender: UIButton) {
+//        
+//        print(sender.tag)
+//        
+//        self.iOSContainerView.isHidden = false
+//        self.androidContainerView.isHidden = true
+//        self.ccppContainerView.isHidden = true
+//    }
+//    
+//    @IBAction func btnNavigateToCCppInfoViewContorller(_ sender: UIButton) {
+//        
+//        print(sender.tag)
+//        
+//        self.ccppContainerView.isHidden = false
+//        self.androidContainerView.isHidden = true
+//        self.iOSContainerView.isHidden = true
+//    }
 }
